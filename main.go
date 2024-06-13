5585c93c4e3220f884037f442103558e0b35a2ec
fabian <fbngrm@gmail.com>
2024-06-13 18:10:34 +0200

    rename files

--- main.go
+++ main.go
@@ -1,19 +1,3 @@
 package main
 
-import (
-	"log"
-
-	"github.com/fbngrm/zh-mnemonics/mnemonic"
-	"github.com/fbngrm/zh-mnemonics/pinyin"
-)
-
-func main() {
-	p := "nan1"
-	b := mnemonic.NewBuilder(pinyin.NewTable())
-	s, err := b.GetBase(p)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(s)
-
-}
+func main() {}
